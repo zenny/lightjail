9f1f6077423055d441814a29213ecdad2abb5b6b
myfreeweb <floatboth@me.com>
2014-04-17 17:50:33 +0400

    extract runJailfile

--- ljbuild/main.go
+++ ljbuild/main.go
@@ -19,6 +19,59 @@ type Script struct {
 	Buildscript  string
 }
 
+func main() {
+	if _, err := exec.LookPath("ljspawn"); err != nil {
+		log.Fatal(err)
+	}
+	if _, err := exec.LookPath("mount"); err != nil {
+		log.Fatal(err)
+	}
+	// TODO: option to hide rootdir?
+	var overrideVersion string
+	flag.StringVar(&overrideVersion, "v", "", "Override version")
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 1 {
+		fmt.Printf("usage: ljbuild /path/to/Jailfile\n")
+		os.Exit(2)
+	}
+	runJailfile(args[0], overrideVersion)
+}
+
+func runJailfile(path, overrideVersion string) {
+	script := parseJailfile(readJailfile(path))
+	if overrideVersion != "" {
+		script.Version = overrideVersion
+	}
+	rootDir := getRootDir()
+	worldDir := filepath.Join(rootDir, "worlds", "10.0-test") // TODO: read world version
+	overlayDir := script.getOverlayPath(rootDir)
+	if _, err := os.Stat(overlayDir); err != nil {
+		if os.IsExist(err) {
+			log.Fatalf("Directory already exists: %s\n", overlayDir)
+		}
+	}
+	if err := os.MkdirAll(overlayDir, 0644); err != nil {
+		log.Fatal(err)
+	}
+	mountPoint, err := ioutil.TempDir("", "ljbuild-")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Building %s %s version %s\n", script.Type, script.Path, script.Version)
+	mount("nullfs", "ro", worldDir, mountPoint)
+	mount("unionfs", "rw", overlayDir, mountPoint)
+	ljspawnCmd := exec.Command("ljspawn", "-n", filepath.Base(mountPoint), "-d", mountPoint, "-p", script.Buildscript)
+	ljspawnCmd.Stdout = os.Stdout
+	ljspawnCmd.Stderr = os.Stdout
+	if err := ljspawnCmd.Run(); err != nil {
+		log.Print(err)
+	}
+	unmount(mountPoint)
+	unmount(mountPoint)
+	syscall.Rmdir(mountPoint)
+}
+
 func readJailfile(path string) string {
 	fileSrc, err := ioutil.ReadFile(path)
 	if err != nil {
@@ -53,53 +106,4 @@ func unmount(from string) {
 	if err := unmountCmd.Run(); err != nil {
 		log.Print(err)
 	}
-}
-
-func main() {
-	if _, err := exec.LookPath("ljspawn"); err != nil {
-		log.Fatal(err)
-	}
-	if _, err := exec.LookPath("mount"); err != nil {
-		log.Fatal(err)
-	}
-	// TODO: option to hide rootdir?
-	var overrideVersion string
-	flag.StringVar(&overrideVersion, "v", "", "Override version")
-	flag.Parse()
-	args := flag.Args()
-	if len(args) < 1 {
-		fmt.Printf("usage: ljbuild /path/to/Jailfile\n")
-		os.Exit(2)
-	}
-	script := parseJailfile(readJailfile(args[0]))
-	if overrideVersion != "" {
-		script.Version = overrideVersion
-	}
-	rootDir := getRootDir()
-	worldDir := filepath.Join(rootDir, "worlds", "10.0-RELEASE") // TODO: read world version
-	overlayDir := script.getOverlayPath(rootDir)
-	if _, err := os.Stat(overlayDir); err != nil {
-		if os.IsExist(err) {
-			log.Fatalf("Directory already exists: %s\n", overlayDir)
-		}
-	}
-	if err := os.MkdirAll(overlayDir, 0644); err != nil {
-		log.Fatal(err)
-	}
-	mountPoint, err := ioutil.TempDir("", "ljbuild-")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Building %s %s version %s\n", script.Type, script.Path, script.Version)
-	mount("nullfs", "ro", worldDir, mountPoint)
-	mount("unionfs", "rw", overlayDir, mountPoint)
-	ljspawnCmd := exec.Command("ljspawn", "-n", filepath.Base(mountPoint), "-d", mountPoint, "-p", script.Buildscript)
-	ljspawnCmd.Stdout = os.Stdout
-	ljspawnCmd.Stderr = os.Stdout
-	if err := ljspawnCmd.Run(); err != nil {
-		log.Print(err)
-	}
-	unmount(mountPoint)
-	unmount(mountPoint)
-	syscall.Rmdir(mountPoint)
 }
