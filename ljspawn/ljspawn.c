295f5167f7cc49dc534664b8b76b6cadcc703af0
myfreeweb <floatboth@me.com>
2014-05-01 13:35:50 +0400

    add -0 to ljspawn, use jail_remove instead of killall

--- ljspawn/ljspawn.c
+++ ljspawn/ljspawn.c
@@ -21,6 +21,7 @@
 #include <sys/mman.h>
 #include <sys/file.h>
 #include <arpa/inet.h>
+#include <pwd.h>
 #include <signal.h>
 #include <stdlib.h>
 #include <stdarg.h>
@@ -28,6 +29,7 @@
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 #define DEFAULT_NAME "lj"
 #define DEFAULT_IFACE "lo0"
@@ -39,6 +41,7 @@ char *ip_s = NULL;
 struct in_addr ip;
 char *name = DEFAULT_NAME;
 char *proc = "echo 'Hello world'";
+bool nobody = false;
 pid_t p_fpid;
 
 void die(const char *format, ...) {
@@ -75,17 +78,19 @@ void usage(char *pname) {
   puts("  -d /path/to/dir -- path to the temporary folder where you have your jail environment");
   puts("  -i IP.AD.DR.ESS -- the jail's IPv4 address (optional)");
   puts("  -f interface0 -- the network interface for aliasing the jail's IPv4 address to (optional, 'lo0' by default)");
+  puts("  -0 -- run as an unprivileged user (nobody)");
   puts("  -n name -- the value for setting jailname and hostname (optional, 'lj' by default)");
   puts("  -p '/path/to/process args' -- the shell command to execute in the jail");
 }
 
 void parse_options(int argc, char *argv[]) {
   int c;
-  while ((c = getopt(argc, argv, "d:f:hi:n:p:?")) != -1) {
+  while ((c = getopt(argc, argv, "d:f:hi:0n:p:?")) != -1) {
     switch (c) {
       case 'd': dest   = optarg; break;
       case 'i': ip_s   = optarg; break;
       case 'f': net_if = optarg; break;
+      case '0': nobody = true;   break;
       case 'n': name   = optarg; break;
       case 'p': proc   = optarg; break;
       case '?':
@@ -109,14 +114,8 @@ void wait_and_bleed(pid_t fpid) {
   int status;
   waitpid(fpid, &status, 0);
   llog("Process exited with status %d", status);
-  char jail_id_str[16];
-  safe_snprintf(jail_id_str, 16, "%d", *jail_id);
+  jail_remove(*jail_id); // Make sure there are no orphans in the jail
   munmap(jail_id, sizeof *jail_id);
-  pid_t killall_pid = fork();
-  if (killall_pid == -1) die_errno("Could not fork");
-  if (killall_pid <= 0) { // Child
-    execv("/usr/bin/killall", (char *[]){ "killall", "-9", "-j", jail_id_str, 0 }); // Make sure there are no orphans in the jail
-  }
 }
 
 int run() {
@@ -142,7 +141,10 @@ int run() {
   *jail_id = jresult;
   chdir("/");
   llog("Running container %s in jail %d", dest, jresult);
-  system("echo 'nobody:*:65534:65534:Unprivileged user:/nonexistent:/usr/sbin/nologin' >> /etc/passwd");
+  if (nobody) {
+    system("pw useradd -n nobody -d /nonexistent -s /usr/sbin/nologin 2> /dev/null");
+    setuid(getpwnam("nobody")->pw_uid);
+  }
   return execve("/bin/sh", (char *[]){ "sh", "-c", proc, 0 },
       (char *[]){ "PATH=/usr/local/bin:/usr/local/sbin:/usr/games:/usr/bin:/usr/sbin:/bin:/sbin",
                   "LC_ALL=en_US.UTF-8",
