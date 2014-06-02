dc77c843933e4d688c5acf5ad8c285652380dea2
myfreeweb <floatboth@me.com>
2014-06-02 20:41:29 +0400

    [ljbuild] move Overlay to separate file

--- ljbuild/script.go
+++ ljbuild/script.go
@@ -1,55 +1,9 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/myfreeweb/lightjail/util"
-	"io/ioutil"
-	"path/filepath"
 	"strings"
 )
-
-type Overlay struct {
-	Name    string
-	Version string
-	From    *Overlay
-}
-
-func (overlay *Overlay) Save(path string) {
-	bytes, err := json.Marshal(overlay)
-	if err != nil {
-		panic(err)
-	}
-	err = ioutil.WriteFile(path, bytes, 0444)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func ReadOverlay(path string) Overlay {
-	bytes, err := ioutil.ReadFile(path)
-	if err != nil {
-		panic(err)
-	}
-	var overlay Overlay
-	err = json.Unmarshal(bytes, &overlay)
-	if err != nil {
-		panic(err)
-	}
-	return overlay
-}
-
-func (overlay *Overlay) GetFromPaths(rootDir string) []string {
-	paths := []string{}
-	if overlay.From != nil {
-		unversionedPath := filepath.Join(rootDir, overlay.From.Name)
-		path := filepath.Join(unversionedPath, util.FindMaxVersionFile(unversionedPath, overlay.From.Version))
-		parent := ReadOverlay(filepath.Join(path, "overlay.json"))
-		paths = append(paths, parent.GetFromPaths(rootDir)...)
-		paths = append(paths, path)
-	}
-	return paths
-}
 
 type Script struct {
 	Overlay
